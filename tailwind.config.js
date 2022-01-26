3369778e068463daf2680da0610f846833c43c4a
Nuran Jafarov <nuran.jafarov@gmail.com>
2022-01-26 17:49:12 +0100

    Added raisinBlack and snow to tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        raisinBlack: '#252627',
+        snow: '#FFF9FB',
+      },
+    },
   },
   plugins: [],
 };
